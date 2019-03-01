56c5ad5ed383e0781c5b6cc44d757ccc68a9a38a
52535465S <52535465S@DES-62396.trabajo.dom>
2019-03-01 08:55:45 +0100

    nuevas funcionalidades

--- holaspringmvc/src/main/java/net/rodor/springmvc/HelloController.java
+++ holaspringmvc/src/main/java/net/rodor/springmvc/HelloController.java
@@ -1,7 +1,9 @@
 package net.rodor.springmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -24,6 +26,23 @@ public class HelloController {
 		tp.setDescripcion("Descripcion permiso Y");
 		modelAndView.addObject("tp", tp);
 		
+		return modelAndView;
+	}
+	
+	@RequestMapping("/helloZZZOI")
+	public ModelAndView helloZZZOI(){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("tipoPermisoAdd");
+		return modelAndView;
+	}
+	
+	@RequestMapping(value="/addTipoPermiso", method=RequestMethod.POST)
+	public ModelAndView addTipoPermiso(@ModelAttribute("tp") TipoPermiso tp){
+		System.out.println(tp);
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("tipoPermisoAdd");
+		modelAndView.addObject("tp", tp);
+		modelAndView.addObject("msg", "Tipo Permiso Añadido.");
 		return modelAndView;
 	}
 }
---
+++ holaspringmvc/src/main/webapp/WEB-INF/views/tipoPermisoAdd.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Mi titulo aqui...</title>
+</head>
+<body>
+<!-- Mensaje -->
+<%=request.getAttribute("msg") %>
+<%=request.getAttribute("tp") %>
+
+<!--  formulario add tipo permiso -->
+<form action="addTipoPermiso" method="post">
+<pre>
+Id: <input type="text" name="id" value="id"/>
+Nombre: <input type="text" name="nombre" value="nombre" />
+descripcion: <input type="text" name="descripcion" value="nombre" />
+<input type="submit" name="Add Nuevo Tipo Permiso" value="Add Nuevo Tipo Permiso"/>
+</pre>
+</form>
+
+</body>
+</html>
