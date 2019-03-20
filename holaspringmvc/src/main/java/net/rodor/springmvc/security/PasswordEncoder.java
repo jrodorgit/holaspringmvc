1a5fa3355ac5e56cc2cb8b35c701d05fb4ada48a
52535465S <52535465S@DES-62396.trabajo.dom>
2019-03-20 09:27:25 +0100

    correccion de vulnerabilidades?

--- holaspringmvc/src/main/java/net/rodor/springmvc/security/PasswordEncoder.java
+++ holaspringmvc/src/main/java/net/rodor/springmvc/security/PasswordEncoder.java
@@ -1,19 +1,11 @@
 package net.rodor.springmvc.security;
 
-import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 public class PasswordEncoder {
 
 	public static void main(String[] args) {
 		
-		ShaPasswordEncoder encoder = new ShaPasswordEncoder(256);
-		System.out.println(encoder.encodePassword("password", null));
-		
-		StandardPasswordEncoder encoderStd = new StandardPasswordEncoder();
-		System.out.println(encoderStd.encode("password"));
-
 		BCryptPasswordEncoder encoderBc = new BCryptPasswordEncoder();
 		System.out.println(encoderBc.encode("password"));
 		System.out.println(encoderBc.encode("password"));
